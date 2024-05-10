002de253ca59125721c91211b7965d8128f194c6
Sandrabua <sandraruperez89@gmail.com>
2024-05-11 01:27:57 +0200

    pagination reset with new search

--- proyecto_next14_pfg/components/app.components/SearchBar.tsx
+++ proyecto_next14_pfg/components/app.components/SearchBar.tsx
@@ -2,7 +2,7 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { useDebouncedCallback } from 'use-debounce';
+import { useDebouncedCallback } from "use-debounce";
 
 const SearchBar = ({ placeholder }: { placeholder: string }) => {
   //se usa useSearchParams porque el componente es de cliente - searchParams es para servidor
@@ -14,15 +14,16 @@ const SearchBar = ({ placeholder }: { placeholder: string }) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set('query', term);
+      if (term !== params.get("query")) {
+        params.delete("page");
+      }
+      params.set("query", term);
     } else {
-      params.delete('query');
+      params.delete("query");
+      params.delete("page");
     }
     replace(`${pathname}?${params.toString()}`);
-
   }, 300);
-
-  
 
   return (
     <div className="relative flex flex-1 flex-shrink-0 ">
@@ -35,7 +36,7 @@ const SearchBar = ({ placeholder }: { placeholder: string }) => {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>

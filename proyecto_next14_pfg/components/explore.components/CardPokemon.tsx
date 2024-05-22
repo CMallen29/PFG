a1deceff8f940f28f3522df59f33770553f363cd
MallenDev <mallensound@gmail.com>
2024-05-22 22:26:38 +0200

    diseño carta

--- proyecto_next14_pfg/components/explore.components/CardPokemon.tsx
+++ proyecto_next14_pfg/components/explore.components/CardPokemon.tsx
@@ -8,7 +8,7 @@ async function CardPokemon({
   dataPokemon: Promise<PokemonSimple[]>;
 }) {
   return (
-    <div className="flex flex-row flex-wrap justify-center items-center gap-12 p-4">
+    <div className="flex flex-row flex-wrap justify-center items-center gap-12 p-4 font-bold">
       {(await dataPokemon).length === 0 && (
         <p>
           Vaya... Parece que el Pokémon que buscas no ha sido descubierto aún
@@ -17,24 +17,33 @@ async function CardPokemon({
       {(await dataPokemon).map((pokemon) => {
         return (
           <PokemonInfo name={pokemon.name}>
-            <div key={pokemon.id} className="bg-greenUnify-500 p-4 rounded-xl w-fit ">
-              <div className="bg-gradient-to-b from-yellowUnify-800 to-100%">
-              <p className="capitalize">{pokemon.name}</p>
-              <p>ID: {pokemon.id}</p>
+            <div
+              key={pokemon.id}
+              className="bg-greenUnify-500 rounded-xl w-48 grid justify-items-center  "
+            >
+              <div className="flex justify-between bg-yellowUnify-800 text-greenUnify-800 border-b-4 border-greenUnify-900/90 rounded-t-xl p-4 w-full text-xl">
+                <p className="capitalize">{pokemon.name}</p>
+                <p className="">{pokemon.id}</p>
               </div>
-             
+
               <img
                 src={pokemon.sprites.other["official-artwork"].front_default}
                 alt=""
-                width="150"
-                height="150"
-                className="m-2 drop-shadow-2xl hover:scale-125"
+                width="170"
+                height="170"
+                className=" m-2 hover:scale-150 transition duration-500 ease-in-out drop-shadow-[3px_3px_rgba(0,0,0)]"
               />
-              {pokemon.types.map((value) => (
-                <span key={value.type.name} className={`bg-filter-${value.type.name} p-2 m-1 rounded-xl capitalize `}>
-                  {value.type.name}
-                </span>
-              ))}
+              <div className="flex flex-row justify-center w-full p-2">
+                {pokemon.types.map((value) => (
+                  <div
+                    key={value.type.name}
+                    className={`bg-filter-${value.type.name} p-2 m-1 rounded-xl capitalize text-center w-full border-greenUnify-900/90 border-2`}
+                  >
+                    
+                    <span className="drop-shadow-[2px_2px_rgba(0,0,0)]">{value.type.name}</span>
+                  </div>
+                ))}
+              </div>
             </div>
           </PokemonInfo>
         );

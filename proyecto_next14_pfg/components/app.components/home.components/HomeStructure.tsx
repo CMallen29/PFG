c901416ce6bffe2092851144b7edf3d4e8746ae6
MallenDev <mallensound@gmail.com>
2024-06-02 15:05:38 +0200

    titulo y pokemon dle dia funcionales

--- proyecto_next14_pfg/components/app.components/home.components/HomeStructure.tsx
+++ proyecto_next14_pfg/components/app.components/home.components/HomeStructure.tsx
@@ -1,29 +1,32 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import RelevantPokemon from "./RelevantPokemon";
+import Welcome from "./Welcome";
+import CarouselPokemon from "./CarouselPokemon";
 
 async function HomeStructure() {
   const session = await getServerSession(authOptions);
 
   return (
-    <div className="grid grid-cols-1 items-center text-white">
-      <div className="flex flex-col justify-center items-center text-2xl ">
-        {session?.user ? (
-          <h2>
-            Inicio de sesion correcto, bienvenido {session?.user.username}
-          </h2>
-        ) : (
-          <h2>Bienvenido</h2>
-        )}
+    <div className="grid grid-cols-1 text-white">
+      <div className="flex flex-cols justify-center items-center w-full">
+        <div className="flex gap-4 items-center text-2xl p-12 bg-greenUnify-900/90 w-4/5 mt-10 rounded-xl">
+          {session?.user ? (
+            <h2 className="bg-greenUnify-500 p-6 rounded-xl mr-6">
+              Bienvenido de nuevo {session.user.email}
+            </h2>
+          ) : (
+            <h2>Bienvenido a Unify</h2>
+          )}
+          <Welcome />
+        </div>
       </div>
-      <div>
-        Pokemon destacado
-      </div>
-      <div>
-        Carrusel
-      </div>
-      <div>
-        Info página
-      </div>
+      <section>
+        <RelevantPokemon />
+      </section>
+      <section>
+        <CarouselPokemon />
+      </section>
     </div>
   );
 }

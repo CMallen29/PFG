90ad690589debe9f1d6af513f95f29fa7738291e
Sandrabua <sandraruperez89@gmail.com>
2024-06-02 18:20:04 +0200

    session server change

--- proyecto_next14_pfg/components/explore.components/CardPokemon.tsx
+++ proyecto_next14_pfg/components/explore.components/CardPokemon.tsx
@@ -2,12 +2,18 @@ import { PokemonSimple } from "@/types/pokemon.types";
 import React from "react";
 import PokemonInfo from "../pokemonInfo.components/PokemonInfo";
 import { ToggleFavorite } from "./ToggleFavorite";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 async function CardPokemon({
   dataPokemon,
 }: {
   dataPokemon: Promise<PokemonSimple[]>;
 }) {
+  const sessionFav = await getServerSession(authOptions).then((session) => session?.user.favorite);
+  console.log(sessionFav);
+  
+  
   return (
     <div className="flex flex-row flex-wrap justify-center items-center gap-12 p-4 font-bold">
       {(await dataPokemon).length === 0 && (
@@ -18,7 +24,7 @@ async function CardPokemon({
       {(await dataPokemon).map((pokemon) => {
         return (
           <div>
-            <ToggleFavorite id={pokemon.id}/>
+            <ToggleFavorite id={pokemon.id} sessionFav={sessionFav}/>
             <PokemonInfo pokemon={pokemon}/>
           </div>
         );
--- proyecto_next14_pfg/components/explore.components/ToggleFavorite.tsx
+++ proyecto_next14_pfg/components/explore.components/ToggleFavorite.tsx
@@ -3,11 +3,15 @@
 import { useRouter } from "next/navigation";
 import { Toggle } from "@/components/ui/toggle";
 import { StarIcon } from "@heroicons/react/16/solid";
-import { useSession } from "next-auth/react";
 
-export function ToggleFavorite({ id }: { id: number }) {
+export function ToggleFavorite({
+  id,
+  sessionFav,
+}: {
+  id: number;
+  sessionFav: number[] | undefined;
+}) {
   const router = useRouter();
-  const { data: session, update } = useSession();
 
   async function handleFavorite() {
     const response = await fetch("/api/updateFavorite", {
@@ -19,10 +23,6 @@ export function ToggleFavorite({ id }: { id: number }) {
         pokemonID: id,
       }),
     });
-
-    console.log("antes", session?.user.favorite);
-    update({ favorite: session?.user.favorite });
-    console.log("despues", session?.user.favorite);
 
     if (response.ok) {
       router.refresh();
@@ -36,15 +36,11 @@ export function ToggleFavorite({ id }: { id: number }) {
     <Toggle
       key={id}
       aria-label="Toggle"
-      defaultPressed={session?.user.favorite.includes(id)}
+      defaultPressed={sessionFav?.includes(id)}
       onPressedChange={() => {
         handleFavorite();
-        console.log("3", session?.user.favorite);
       }}
     >
-      {session?.user.favorite.includes(id)
-        ? "Quitar de favoritos"
-        : "Añadir a favoritos"}
       <StarIcon className="w-6 h-6" />
     </Toggle>
   );

dca9c61c531fe50566fc39b9a3c56844b5c308fa
Sandrabua <sandraruperez89@gmail.com>
2024-06-05 01:04:49 +0200

    card pokemon minor fix

--- proyecto_next14_pfg/components/explore.components/CardPokemon.tsx
+++ proyecto_next14_pfg/components/explore.components/CardPokemon.tsx
@@ -11,58 +11,58 @@ async function CardPokemon({
 }: {
   dataPokemon: Promise<Pokemon[]>;
 }) {
+  const dataPokemonCard = await dataPokemon;
   const sessionFav = await getServerSession(authOptions).then(
     (session) => session?.user.favorite
   );
 
   return (
     <div className="flex flex-row flex-wrap justify-center items-center gap-12 p-4 font-bold">
-      {(await dataPokemon).length === 0 && (
+      {dataPokemonCard.length === 0 ? (
         <p>
           Vaya... Parece que el Pokémon que buscas no ha sido descubierto aún
         </p>
-      )}
-      {(await dataPokemon).map((pokemon) => {
-        return (
-          <div key={pokemon.id}>
-            <ToggleFavorite id={pokemon.id} sessionFav={sessionFav} />
-            <Link href={`/pokemon/${pokemon.name}`}>
-              <div
-                className="bg-greenUnify-500 rounded-xl w-48 grid justify-items-center  "
-              >
-                <div className="flex justify-between bg-yellowUnify-800 text-greenUnify-800 border-b-4 border-greenUnify-900/90 rounded-t-xl p-4 w-full text-xl">
-                  <p className="capitalize">
-                    {pokemon.name.replaceAll("-", " ")}
-                  </p>
-                  <p className="">{pokemon.id}</p>
-                </div>
+      ) : (
+        dataPokemonCard.map((pokemon) => {
+          return (
+            <div key={pokemon.id}>
+              <ToggleFavorite id={pokemon.id} sessionFav={sessionFav} />
+              <Link href={`/pokemon/${pokemon.name}`}>
+                <div className="bg-greenUnify-500 rounded-xl w-48 grid justify-items-center  ">
+                  <div className="flex justify-between bg-yellowUnify-800 text-greenUnify-800 border-b-4 border-greenUnify-900/90 rounded-t-xl p-4 w-full text-xl">
+                    <p className="capitalize">
+                      {pokemon.name.replaceAll("-", " ")}
+                    </p>
+                    <p className="">{pokemon.id}</p>
+                  </div>
 
-                <img
-                  src={getImagePokemon(
-                    pokemon.sprites.other["official-artwork"].front_default
-                  )}
-                  alt=""
-                  width="170"
-                  height="170"
-                  className=" m-2 hover:scale-150 transition duration-500 ease-in-out drop-shadow-[3px_3px_rgba(0,0,0)]"
-                />
-                <div className="flex flex-row justify-center w-full p-2">
-                  {pokemon.types.map((value) => (
-                    <div
-                      key={value.type.name}
-                      className={`bg-filter-${value.type.name} p-2 m-1 rounded-xl capitalize text-center w-full border-greenUnify-900/90 border-2`}
-                    >
-                      <span className="drop-shadow-[2px_2px_rgba(0,0,0)]">
-                        {value.type.name}
-                      </span>
-                    </div>
-                  ))}
+                  <img
+                    src={getImagePokemon(
+                      pokemon.sprites.other["official-artwork"].front_default
+                    )}
+                    alt=""
+                    width="170"
+                    height="170"
+                    className=" m-2 hover:scale-150 transition duration-500 ease-in-out drop-shadow-[3px_3px_rgba(0,0,0)]"
+                  />
+                  <div className="flex flex-row justify-center w-full p-2">
+                    {pokemon.types.map((value) => (
+                      <div
+                        key={value.type.name}
+                        className={`bg-filter-${value.type.name} p-2 m-1 rounded-xl capitalize text-center w-full border-greenUnify-900/90 border-2`}
+                      >
+                        <span className="drop-shadow-[2px_2px_rgba(0,0,0)]">
+                          {value.type.name}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </div>
-        );
-      })}
+              </Link>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }

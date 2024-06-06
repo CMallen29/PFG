8af790b635a8f27739eef2598b02b75f51c2b449
Sandrabua <sandraruperez89@gmail.com>
2024-06-06 11:34:06 +0200

    change 404 image

--- proyecto_next14_pfg/app/not-found.tsx
+++ proyecto_next14_pfg/app/not-found.tsx
@@ -4,10 +4,11 @@ export default function NotFound() {
   return (
     <main className="flex h-full flex-col items-center justify-center gap-2">
       <Image
-        src="/pokemon/MissingNo.png"
-        width={100}
-        height={100}
+        src="/pokemon/MissingNo.jpg"
+        width={300}
+        height={300}
         alt="Missingno"
+        className="rounded-xl shadow-2xl shadow-warningUnify-900 "
       />
       <h2 className="text-5xl font-semibold text-yellowUnify-800">
         404 Not Found
